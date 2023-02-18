07c957c4150ff035d59d40eb71b34b6d0653fd4f
Mad <madalin.cucu.99@gmail.com>
2023-02-18 19:59:01 +0200

    Finished js

---
+++ index.js
@@ -0,0 +1,23 @@
+const containerEl = document.querySelector(".container");
+
+const careers = ["YouTuber", "Web Developer", "Freelancer", "Instructor"];
+
+let careerIndex = 0;
+
+let characterIndex = 0;
+
+function updateText(){
+  containerEl.innerHTML = `<h1>I am a ${careers[careerIndex].slice(0,characterIndex)}</h1>`;
+  characterIndex++
+  if(characterIndex === careers[careerIndex].length){
+    careerIndex++;
+    characterIndex = 0;
+  }
+
+  if(careerIndex === careers.length){
+    careerIndex = 0;
+  }
+  setTimeout(updateText,400)
+}
+
+updateText()
